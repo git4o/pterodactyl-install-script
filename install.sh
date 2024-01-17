042b487a427759ac13afb284dd68e376b70dc321
git4o <71034923+git4o@users.noreply.github.com>
2024-01-16 17:45:07 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,6 @@ curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 apt update
 
 # Install Dependencies
-apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+sudo apt-get install php8.1 php8.1-common php8.1-cli php8.1-gd php8.1-mysql php8.1-mbstring php8.1-bcmath php8.1-xml php8.1-fpm php8.1-curl php8.1-zip
+
+sudo apt-get install mariadb-server nginx tar unzip git redis-server
